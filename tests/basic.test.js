14ec4225df761faff697f9c951ca6c6faebe4605
Vishnubalaji-C <vishnubalajiit@gmail.com>
2026-09-02 13:28:06 +0530

    Test CI CD deployment flow

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,7 +1,7 @@
 const test = require('node:test');
 const assert = require('node:assert');
 
-test('GiveWay basic application test', () => {
+test('GiveWay CI CD pipeline test', () => {
   const appName = 'GiveWay';
 
   assert.strictEqual(appName, 'GiveWay');
